228fb19afae57da76427486ab89f3ce10e8e65aa
Harman <harman@Harmans-MacBook-Air.local>
2024-11-14 15:29:13 +0530

    fix header or footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,8 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
+import MainHeader from "@/components/Header";
+import Footer from "@/components/footer";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -52,7 +54,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Nav />
+          <MainHeader />
           <Main>{children}</Main>
           <Footer />
         </ThemeProvider>
@@ -62,108 +64,108 @@ export default function RootLayout({
   );
 }
 
-const Nav = ({ className, children, id }: NavProps) => {
-  return (
-    <nav
-      className={cn(
-        "sticky z-50 top-0 bg-background",
-        "border-b",
-        "fade-in",
-        className,
-      )}
-      id={id}
-    >
-      <div
-        id="nav-container"
-        className="max-w-5xl mx-auto py-4 px-6 sm:px-8 flex justify-between items-center"
-      >
-        <Link
-          className="hover:opacity-75 transition-all flex gap-2 items-center"
-          href="/"
-        >
-          <h2 className="sr-only">next-wp starter</h2>
-          <Image
-            src={Logo}
-            alt="Logo"
-            className="dark:invert"
-            width={84}
-            height={30.54}
-          ></Image>
-        </Link>
-        {children}
-        <div className="flex items-center gap-2">
-          <div className="mx-2 hidden md:flex">
-            {Object.entries(mainMenu).map(([key, href]) => (
-              <Button key={href} asChild variant="ghost" size="sm">
-                <Link href={href}>
-                  {key.charAt(0).toUpperCase() + key.slice(1)}
-                </Link>
-              </Button>
-            ))}
-          </div>
-          <Button asChild className="hidden sm:flex">
-            <Link href="https://github.com/9d8dev/next-wp">Get Started</Link>
-          </Button>
-          <MobileNav />
-        </div>
-      </div>
-    </nav>
-  );
-};
+// const Nav = ({ className, children, id }: NavProps) => {
+//   return (
+//     <nav
+//       className={cn(
+//         "sticky z-50 top-0 bg-background",
+//         "border-b",
+//         "fade-in",
+//         className,
+//       )}
+//       id={id}
+//     >
+//       <div
+//         id="nav-container"
+//         className="max-w-5xl mx-auto py-4 px-6 sm:px-8 flex justify-between items-center"
+//       >
+//         <Link
+//           className="hover:opacity-75 transition-all flex gap-2 items-center"
+//           href="/"
+//         >
+//           <h2 className="sr-only">next-wp starter</h2>
+//           <Image
+//             src={Logo}
+//             alt="Logo"
+//             className="dark:invert"
+//             width={84}
+//             height={30.54}
+//           ></Image>
+//         </Link>
+//         {children}
+//         <div className="flex items-center gap-2">
+//           <div className="mx-2 hidden md:flex">
+//             {Object.entries(mainMenu).map(([key, href]) => (
+//               <Button key={href} asChild variant="ghost" size="sm">
+//                 <Link href={href}>
+//                   {key.charAt(0).toUpperCase() + key.slice(1)}
+//                 </Link>
+//               </Button>
+//             ))}
+//           </div>
+//           <Button asChild className="hidden sm:flex">
+//             <Link href="https://github.com/9d8dev/next-wp">Get Started</Link>
+//           </Button>
+//           <MobileNav />
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
 
-const Footer = () => {
-  return (
-    <footer>
-      <Section>
-        <Container className="grid md:grid-cols-[1.5fr_0.5fr_0.5fr] gap-12">
-          <div className="flex flex-col gap-6 not-prose">
-            <Link href="/">
-              <h3 className="sr-only">brijr/components</h3>
-              <Image
-                src={Logo}
-                alt="Logo"
-                width={120}
-                height={27.27}
-                className="dark:invert hover:opacity-75 transition-all"
-              ></Image>
-            </Link>
-            <p>
-              <Balancer>{metadata.description}</Balancer>
-            </p>
-          </div>
-          <div className="flex flex-col gap-2 text-sm">
-            <h5 className="font-medium text-base">Website</h5>
-            {Object.entries(mainMenu).map(([key, href]) => (
-              <Link
-                className="hover:underline underline-offset-4"
-                key={href}
-                href={href}
-              >
-                {key.charAt(0).toUpperCase() + key.slice(1)}
-              </Link>
-            ))}
-          </div>
-          <div className="flex flex-col gap-2 text-sm">
-            <h5 className="font-medium text-base">Blog</h5>
-            {Object.entries(contentMenu).map(([key, href]) => (
-              <Link
-                className="hover:underline underline-offset-4"
-                key={href}
-                href={href}
-              >
-                {key.charAt(0).toUpperCase() + key.slice(1)}
-              </Link>
-            ))}
-          </div>
-        </Container>
-        <Container className="border-t not-prose flex flex-col md:flex-row md:gap-2 gap-6 justify-between md:items-center">
-          <ThemeToggle />
-          <p className="text-muted-foreground">
-            © <a href="https://9d8.dev">9d8</a>. All rights reserved.
-            2024-present.
-          </p>
-        </Container>
-      </Section>
-    </footer>
-  );
-};
+// const Footer = () => {
+//   return (
+//     <footer>
+//       <Section>
+//         <Container className="grid md:grid-cols-[1.5fr_0.5fr_0.5fr] gap-12">
+//           <div className="flex flex-col gap-6 not-prose">
+//             <Link href="/">
+//               <h3 className="sr-only">brijr/components</h3>
+//               <Image
+//                 src={Logo}
+//                 alt="Logo"
+//                 width={120}
+//                 height={27.27}
+//                 className="dark:invert hover:opacity-75 transition-all"
+//               ></Image>
+//             </Link>
+//             <p>
+//               <Balancer>{metadata.description}</Balancer>
+//             </p>
+//           </div>
+//           <div className="flex flex-col gap-2 text-sm">
+//             <h5 className="font-medium text-base">Website</h5>
+//             {Object.entries(mainMenu).map(([key, href]) => (
+//               <Link
+//                 className="hover:underline underline-offset-4"
+//                 key={href}
+//                 href={href}
+//               >
+//                 {key.charAt(0).toUpperCase() + key.slice(1)}
+//               </Link>
+//             ))}
+//           </div>
+//           <div className="flex flex-col gap-2 text-sm">
+//             <h5 className="font-medium text-base">Blog</h5>
+//             {Object.entries(contentMenu).map(([key, href]) => (
+//               <Link
+//                 className="hover:underline underline-offset-4"
+//                 key={href}
+//                 href={href}
+//               >
+//                 {key.charAt(0).toUpperCase() + key.slice(1)}
+//               </Link>
+//             ))}
+//           </div>
+//         </Container>
+//         <Container className="border-t not-prose flex flex-col md:flex-row md:gap-2 gap-6 justify-between md:items-center">
+//           <ThemeToggle />
+//           <p className="text-muted-foreground">
+//             © <a href="https://9d8.dev">9d8</a>. All rights reserved.
+//             2024-present.
+//           </p>
+//         </Container>
+//       </Section>
+//     </footer>
+//   );
+// };
